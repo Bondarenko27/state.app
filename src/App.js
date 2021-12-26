ebd249a959c8725cb6cb3f9e27458b72ff65dff8
Bondarenko27 <bondarenkokristina27@gmail.com>
2021-12-25 20:49:17 -0500

    useReducer example

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React  from 'react';
-import State from './State';
+//import State from './State';
 import { useState } from 'react';
-
+import Reducer from './Reducer';
 
 function App() {
   return (
     <div className="App">
       
-     <State/>
+     <Reducer/>
     </div>
   );
 }
