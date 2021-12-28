79c63119579297e338321308cab5769b0da48a1a
Bondarenko27 <bondarenkokristina27@gmail.com>
2021-12-27 19:05:17 -0500

    useEffect example

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React  from 'react';
 //import State from './State';
 import { useState } from 'react';
-import Reducer from './Reducer';
+import Effect from './useEffect';
+//import Reducer from './Reducer';
 
 function App() {
   return (
     <div className="App">
       
-     <Reducer/>
+     <Effect/>
     </div>
   );
 }
